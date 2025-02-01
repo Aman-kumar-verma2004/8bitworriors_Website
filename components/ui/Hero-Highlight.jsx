408761ea1857e5458ecadff39b2912530fb8e836
amrit <amritkumarverma848@gmail.com>
2025-02-01 16:48:30 +0530

    Necesssary Updates

--- components/ui/Hero-Highlight.jsx
+++ components/ui/Hero-Highlight.jsx
@@ -54,11 +54,11 @@ const HeroSecton = () => {
                     <div className="relative">
                         <div className="lg:w-2/3">
                             <p className="text-sm font-normal tracking-widest text-gray-300 uppercase">A Hub Where Tech Meets Creativity!</p>
-                            <h1 className="mt-6 text-4xl font-normal text-white sm:mt-10 sm:text-5xl lg:text-6xl xl:text-8xl"><span className="text-transparent bg-clip-text bg-gradient-to-r from-cyan-500 to-purple-500">Code Like Warriors</span>
+                            <h1 className="mt-6 text-4xl font-normal text-white sm:mt-10 sm:text-5xl lg:text-6xl xl:text-8xl"><span className="text-transparent bg-clip-text bg-gradient-to-r from-green-100 to-green-800">Code Like Warriors</span>
                             </h1>
                             <p className="max-w-lg mt-4 text-xl font-normal text-gray-400 sm:mt-8">Join 8-Bit Warriors and embark on an exciting journey to transform your future in tech!</p>
                             <div className="relative inline-flex items-center justify-center mt-8 sm:mt-12 group">
-                                <div className="absolute transition-all duration-200 rounded-full -inset-px bg-gradient-to-r from-cyan-500 to-purple-500 group-hover:shadow-lg group-hover:shadow-cyan-500/50"></div>
+                                <div className="absolute transition-all duration-200 rounded-full -inset-px bg-gradient-to-r from-green-100 to-green-700-500 group-hover:shadow-lg group-hover:shadow-green-500/50"></div>
                                 <a href="#" title="" className="relative inline-flex items-center justify-center px-8 py-3 text-base font-normal text-white bg-black border border-transparent rounded-full" role="button">Learn more</a>
                             </div>
 
@@ -80,7 +80,7 @@ const HeroSecton = () => {
                         </div>
 
                         <div className="mt-8 md:absolute md:mt-0 md:top-32 lg:top-0 md:right-0">
-                            <img className="w-full max-w-3xl mx-auto lg:max-w-4xl xl:max-w-5xl h-auto lg:h-auto xl:h-auto" src="/1stfinal.png" alt="" />
+                            <img className="w-full max-w-2xl mx-auto lg:max-w-3xl xl:max-w-xxl h-auto lg:h-auto 4xl:h-auto object-contain" src="/landingpagepic.png" alt="" />
                         </div>
                     </div>
                 </div>
